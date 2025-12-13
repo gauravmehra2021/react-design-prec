22b92e7a7ad2d1b2c3489ef96d641763a7e0c04a
gauravmehra2021 <gooravmehra@gmail.com>
2025-12-14 00:57:30 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './App.css'
+import logo from '.././public/images/Logo.png'
+
 const App = () => {
   return (
   <div className="dashboard">
-      <header className="header">Header</header>
+      <header className="header">
+
+<div className='headers-main'>
+  <div className='logo'>
+    <img  src={logo}/>
+  </div>
+   <div className='nav'>
+<ul class="nav-menu">
+  <li class="nav-item">
+    <a href="#" class="nav-link">Home</a>
+  </li>
+  <li class="nav-item">
+    <a href="#" class="nav-link">Services</a>
+  </li>
+  <li class="nav-item">
+    <a href="#" class="nav-link">Contact</a>
+  </li>
+</ul>
+
+
+
+   </div>
+    <div className='profile'>profile</div>
+</div>
+
+      </header>
 
       <aside className="sidebar">Sidebar</aside>
 
