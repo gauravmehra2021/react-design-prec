4e3e5dfb8dce7be1d79c7960f81bd7900ac681aa
gauravmehra2021 <gooravmehra@gmail.com>
2025-12-19 14:13:36 +0530

    added the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import LayoutPRec from './pages/layout/LayoutPRec'
 // import Navbars from './pages/navbar/Navbars'
 // import CenterDiv from './pages/centerdiv/CenterDiv'
-import Creative from './pages/creative/Creative'
+// import Creative from './pages/creative/Creative'
 
 const App = () => {
   return (
     <div>
       {/* <Navbars/> */}
       {/* <CenterDiv/> */}
-      <Creative/>
+      {/* <Creative/> */}
+      <LayoutPRec/>
     </div>
   )
 }
---
+++ src/pages/layout/LayoutPrec.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './LayoutPRec.css'
+const LayoutPRec = () => {
+    return (
+        <div className='main'>
+
+            <div className='headers'>Headers</div>
+            <div className='sidebar' >Sidebar</div>
+            <div className='main-content' >Main-content
+               <div className="maincard">
+  <div className="card">Card 1</div>
+  <div className="card">Card 2</div>
+  <div className="card">Card 3</div>
+  <div className="card">Card 4</div>
+  <div className="card">Card 5</div>
+  <div className="card">Card 6</div>
+  <div className="card">Card 7</div>
+  <div className="card">Card 8</div>
+</div>
+
+
+            </div>
+            <div className='footer'>Footer</div>
+        </div>
+    )
+}
+
+export default LayoutPRec
